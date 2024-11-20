e7e0a0358385910226396d7641969a21c783a8e9
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2024-11-20 19:52:46 -0300

    initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+module.exports = {
+  ColorWhite: "#ffffff",
+  ColorGray200: "#c1c1c4",
+  ColorGray500: "#606060",
+  ColorGray800: "#1c1c1f",
+  ColorGray900: "#121214",
+  ColorPurple500: "#8257e5",
+};
